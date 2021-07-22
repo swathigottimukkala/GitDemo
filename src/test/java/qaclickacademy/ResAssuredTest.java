90e7b7f32b68d97fbc769c087b82201becb7cee7
swathigottimukkala <swathi.gottimukkala@gmail.com>
2021-07-22 08:34:27 +0530

    Asian Time zone

--- src/test/java/qaclickacademy/ResAssuredTest.java
+++ src/test/java/qaclickacademy/ResAssuredTest.java
@@ -7,6 +7,8 @@ public class ResAssuredTest {
 		@Test
 		public void RestAssuredCode() {
 			System.out.println("RestAssuredCode");
+			System.out.println("PostJira1");
+			System.out.println("PostJira2	");
 		}
 
 	}
